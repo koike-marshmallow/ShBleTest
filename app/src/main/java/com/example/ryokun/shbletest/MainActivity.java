426797d0e86f55e25ec643f1194fd000a078bea6
takaaki koike <taka.vao.321@gmail.com>
2016-05-26 21:27:34 +0900

    作れるところまで作ってみた

--- app/src/main/java/com/example/ryokun/shbletest/MainActivity.java
+++ app/src/main/java/com/example/ryokun/shbletest/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.ryokun.shbletest;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private BluetoothManager btm;
+    private BluetoothAdapter bta;
+    private BluetoothAdapter.LeScanCallback lesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btm = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        bta = btm.getAdapter();
+
+
+        lesc = new BluetoothAdapter.LeScanCallback(){
+            @Override
+            public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord){
+                String msg = "ADDRESS=" + device.getAddress() + "\nRSSI=" + rssi;
+                Log.d("BLE", msg);
+            }
+        };
+    }
+
+    public void onBtnStartBleScanClicked(View view){
+        bta.startLeScan(lesc);
+    }
+
+    public void onBtnStopBleScanClicked(View view){
+        bta.stopLeScan(lesc);
     }
 }
+
