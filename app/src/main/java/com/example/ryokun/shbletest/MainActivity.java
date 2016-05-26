630a9a09315a05e7721db62f0286213f1ae55ac7
takaaki koike <taka.vao.321@gmail.com>
2016-05-26 22:55:36 +0900

    レイアウトを作成

--- app/src/main/java/com/example/ryokun/shbletest/MainActivity.java
+++ app/src/main/java/com/example/ryokun/shbletest/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private BluetoothManager btm;
@@ -22,12 +25,30 @@ public class MainActivity extends AppCompatActivity {
         btm = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
         bta = btm.getAdapter();
 
+        final Button bs = (Button)this.findViewById(R.id.btnstart);
+        final Button be = (Button)this.findViewById(R.id.btnstop);
+        final TextView tvs = (TextView)this.findViewById(R.id.textstatus);
+
+        bs.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                tvs.setText("start");
+                onBtnStartBleScanClicked(v);
+            }
+        });
+        be.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                tvs.setText("stop");
+                onBtnStopBleScanClicked(v);
+            }
+        });
+
 
         lesc = new BluetoothAdapter.LeScanCallback(){
             @Override
             public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord){
                 String msg = "ADDRESS=" + device.getAddress() + "\nRSSI=" + rssi;
                 Log.d("BLE", msg);
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
             }
         };
     }
